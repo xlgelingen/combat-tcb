fa9f4acd5851abd3334b1ee3d1123fbddff52e08
xlgelingen <xuliucg@gmail.com>
2024-05-18 15:34:42 +0800

    feat: my-webo

--- miniprogram/pages/my_topic/my_topic.js
+++ miniprogram/pages/my_topic/my_topic.js
@@ -5,33 +5,54 @@ Page({
   data: {
     fullScreen: false,
     topics: [],
+    loadedTopicsCount: 0,
+    perLoadCount: 10,
   },
-  onShow: function() {
+  onShow: function () {
     this.getTopics()
   },
-  getTopics: function(cb) {
+  getTopics: function (cb) {
+    const { loadedTopicsCount, perLoadCount } = this.data;
     // 加上 where 条件判断，传入筛选条件 openid
     db.collection('topic').orderBy('createTime', 'desc').where({
       _openid: app.globalData.openid,
-    }).get({
-      success: res => {
-        console.log('[数据库] [查询记录] 成功: ', res)
-        let topics = res.data;
-        this.setData({ topics })
-        typeof cb === 'function' && cb();
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
-      }
-    })
+    }).skip(loadedTopicsCount)
+      .limit(perLoadCount).get({
+        success: res => {
+          let topics = res.data;
+          console.log('[数据库] [查询记录] 成功: ', topics)
+          const newLoadedTopicsCount = loadedTopicsCount + topics.length;
+          this.setData({
+            topics: this.data.topics.concat(topics),
+            loadedTopicsCount: newLoadedTopicsCount,
+          });
+          typeof cb === 'function' && cb();
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('[数据库] [查询记录] 失败：', err)
+        }
+      })
   },
-  onPullDownRefresh: function() {
-    this.getTopics(()=>{
-      wx.stopPullDownRefresh()
-    })
-  }
+  onPullDownRefresh: function () {
+    this.setData({
+      loadedTopicsCount: 0,
+      topics: []
+    });
+    this.getTopics(() => {
+      wx.stopPullDownRefresh();
+    });
+  },
+  onReachBottom: function () {
+    wx.showLoading({
+      title: '',
+    });
+    setTimeout(() => {
+      this.getTopics();
+      wx.hideLoading();
+    }, 500);
+  },
 })

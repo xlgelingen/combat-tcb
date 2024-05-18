171e8ae53526ed10752d11b5082e7c7773713655
xlgelingen <xuliucg@gmail.com>
2024-05-18 21:11:52 +0800

    feat: my-reply

--- miniprogram/pages/my_reply/my_reply.js
+++ miniprogram/pages/my_reply/my_reply.js
@@ -1,4 +1,47 @@
+const app = getApp();
+const db = wx.cloud.database()
 Page({
   data: {
+    replies: [],
+    loadedRepliesCount: 0,
+    perLoadCount: 15,
   },
-});
+  onShow: function () {
+    this.getReplies();
+  },
+  getReplies: function () {
+    const { loadedRepliesCount, perLoadCount } = this.data;
+    db.collection('reply').orderBy('createTime', 'desc').where({
+      _openid: app.globalData.openid,
+    }).skip(loadedRepliesCount)
+      .limit(perLoadCount).get({
+        success: (res) => {
+          let replies = res.data;
+          console.log('[数据库] [查询记录] 成功: ', replies)
+          const newloadedRepliesCount = loadedRepliesCount + replies.length;
+          this.setData({
+            replies: this.data.replies.concat(replies),
+            loadedRepliesCount: newloadedRepliesCount,
+          });
+        }
+      })
+  },
+  onPullDownRefresh: function () {
+    this.setData({
+      loadedRepliesCount: 0,
+      replies: []
+    });
+    this.getReplies(() => {
+      wx.stopPullDownRefresh();
+    });
+  },
+  onReachBottom: function () {
+    wx.showLoading({
+      title: '',
+    });
+    setTimeout(() => {
+      this.getReplies();
+      wx.hideLoading();
+    }, 500);
+  },
+})
